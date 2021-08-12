13d1161bd6cf9f8b0dc3b047d907e6a0fd4ce18a
CasperFlyModz <62319070+CasperFlyModz@users.noreply.github.com>
2021-08-11 22:25:07 -0400

    Update functions.lua

--- functions.lua
+++ functions.lua
@@ -2,45 +2,45 @@ local RIP = {}
 RIP.ME = game:GetService("Players").LocalPlayer
 RIP.Char = ME.Character or ME.CharacterAdded:Wait()
 ME.CharacterAdded:Connect(function(v)
-    RIP.Char = v
+	RIP.Char = v
 end)
 function RIP.SetProperty(Obj, Prop, Val)
-    if not RIP.Char:FindFirstChild("PompousTheCloud") then
-        if not RIP.ME.Backpack:FindFirstChild("PompousTheCloud") then
-            return error("You do not own VIP or a cloud in your inventory.")
-        end
-        RIP.ME.Backpack.PompousTheCloud.Parent = RIP.Char
-    end
-    RIP.Char:WaitForChild("PompousTheCloud").ServerControl:InvokeServer("SetProperty",{
-        ["Object"] = Obj,
-        ["Property"] = tostring(Prop),
-        ["Value"] = Val
-    })
-    return Obj
+	if not RIP.Char:FindFirstChild("PompousTheCloud") then
+		if not RIP.ME.Backpack:FindFirstChild("PompousTheCloud") then
+			return error("You do not own VIP or a cloud in your inventory.")
+		end
+		RIP.ME.Backpack.PompousTheCloud.Parent = RIP.Char
+	end
+	RIP.Char:WaitForChild("PompousTheCloud").ServerControl:InvokeServer("SetProperty", {
+		["Object"] = Obj,
+		["Property"] = tostring(Prop),
+		["Value"] = Val
+	})
+	return Obj
 end
 function RIP.GuiEvent()
-    if RIP.Char:FindFirstChildOfClass("Model") then
-        RIP.Char:FindFirstChildOfClass("Model"):Destroy()
-    end
-    workspace.GuiEvent:FireServer("")
-    RIP.SetProperty(RIP.Char:WaitForChild(""):WaitForChild("Head"), "Transparency", 1)
-    RIP.SetProperty(RIP.Char.Head, "Transparency", 0)
-    RIP.Char[""].Head:ClearAllChildren()
-    return RIP.Char[""]
+	if RIP.Char:FindFirstChildOfClass("Model") then
+		RIP.Char:FindFirstChildOfClass("Model"):Destroy()
+	end
+	workspace.GuiEvent:FireServer("")
+	RIP.SetProperty(RIP.Char:WaitForChild(""):WaitForChild("Head"), "Transparency", 1)
+	RIP.SetProperty(RIP.Char.Head, "Transparency", 0)
+	RIP.Char[""].Head:ClearAllChildren()
+	return RIP.Char[""]
 end
 function RIP.Kill(player)
-    RIP.SetProperty(RIP.GuiEvent():WaitForChild("Head"), "Parent", plr.Character or game:GetService("Players"):FindFirstChild(plr).Character)
-    RIP.Char[""]:Destroy()
+	RIP.SetProperty(RIP.GuiEvent():WaitForChild("Head"), "Parent", plr.Character or game:GetService("Players"):FindFirstChild(plr).Character)
+	RIP.Char[""]:Destroy()
 end
 function RIP.EC(bool)
-    if not RIP.Char:FindFirstChild("PompousTheCloud") then
-        if not RIP.ME.Backpack:FindFirstChild("PompousTheCloud") then
-            return error("You do not own VIP or a cloud in your inventory.")
-        end
-        RIP.ME.Backpack.PompousTheCloud.Parent = RIP.Char
-    end
+	if not RIP.Char:FindFirstChild("PompousTheCloud") then
+		if not RIP.ME.Backpack:FindFirstChild("PompousTheCloud") then
+			return error("You do not own VIP or a cloud in your inventory.")
+		end
+		RIP.ME.Backpack.PompousTheCloud.Parent = RIP.Char
+	end
 	RIP.Char:WaitForChild("PompousTheCloud").ServerControl:InvokeServer("Fly", {
-	    ["Flying"] = bool
+		["Flying"] = bool
 	})
 end
 return RIP
